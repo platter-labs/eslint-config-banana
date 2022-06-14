04e313e7a763e0326370dab70f8add286d76697d
Glenn Reyes <glenn@glennreyes.com>
2022-06-14 18:39:21 +0200

    feat: update rules & add node entry point

--- react.js
+++ react.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: [
+    // React config
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:tailwindcss/recommended',
+
+    // Base config
     './index',
   ],
   rules: {
@@ -12,6 +16,9 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
+    // Using classnames order through official prettier plugin instead
+    'tailwindcss/classnames-order': 'off',
+    'tailwindcss/no-custom-classname': 'off',
   },
   settings: {
     react: {
