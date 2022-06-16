605a308aa1d698622581be5f24ce2f91473f23c6
Glenn Reyes <glenn@glennreyes.com>
2022-06-16 20:47:14 +0200

    fix: fix prettier

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
