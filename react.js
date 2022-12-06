f5bef25efd71b9257c66633a255a50ca4517626a
Glenn Reyes <glenn@glennreyes.com>
2022-12-06 10:27:46 +0100

    feat: turn off react/no-unescaped-entities

--- react.js
+++ react.js
@@ -15,10 +15,11 @@ module.exports = {
     'react/jsx-curly-brace-presence': ['error', 'never'],
     'react/jsx-sort-props': 'error',
     'react/jsx-uses-react': 'off',
+    'react/no-unescaped-entities': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/self-closing-comp': 'error',
-    // Using classnames order through official prettier plugin instead
+    // Use classnames order through official prettier plugin instead
     'tailwindcss/classnames-order': 'off',
     'tailwindcss/no-custom-classname': 'off',
   },
